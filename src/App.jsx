c8bd56463fa4712614a9b0dd61b90865e5d35a8b
Tom Roy <tomroy553@gmail.com>
2025-10-20 21:51:35 +0530

    Initial commit: Vite + React setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "4rem" }}>
+      <h1>Welcome to My Vite + React App 🌿</h1>
+      <p>Deployed live via Netlify!</p>
+    </div>
+  );
+}
+export default App;
