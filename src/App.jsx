a7263992fe36534157404ef3da78ba06bf25358c
Tom Roy <tomroy553@gmail.com>
2025-10-20 22:29:25 +0530

    Updated main site with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "4rem" }}>
       <h1>Welcome to My Vite + React App 🌿</h1>
-      <h1>Hello from the Preview 🌿✨</h1>git add .
+      <h1>Hello from the Preview 🌿✨</h1>
       <p>Deployed live via Netlify!</p>
+      <h1>Hello from the Preview 🌿✨</h1>
     </div>
   );
-}
 export default App;
